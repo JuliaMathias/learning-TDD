a2e96b06274df458abebd176c3a1b4b1e450df1d
Julia Mathias <juliamss@gmail.com>
2021-03-03 14:53:23 -0300

    Test if the add method is working

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,14 @@
-calc = Calculator.new
-calc.add(5, 10)
+require "rspec/autorun"
+
+# calc = Calculator.new
+# calc.add(5, 10)
+
+describe Calculator do
+    describe "#add" do
+      it "adds two numbers" do
+        calculator = Calculator.new
+  
+        expect(calculator.add(5, 10)).to eq(15)
+      end
+    end
+  end
