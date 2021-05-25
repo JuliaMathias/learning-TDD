6d3f15a47982d81a0878bffef3eb3e1a4c158c99
JuliaMathias <juliamss@gmail.com>
2021-05-25 19:43:46 -0300

    refactor unit converter

--- unit_converter/unit_converter.rb
+++ unit_converter/unit_converter.rb
@@ -1,26 +1,30 @@
-require_relative "quantity"
+# frozen_string_literal: true
 
+require_relative 'quantity'
+
+# Holds the Unit Converter Behaviour
 class UnitConverter
-    def initialize(initial_quantity, target_unit, conversion_database)
-        @initial_quantity = initial_quantity
-        @target_unit = target_unit
-        @conversion_database = conversion_database
-    end
+  def initialize(initial_quantity, target_unit, conversion_database)
+    @initial_quantity = initial_quantity
+    @target_unit = target_unit
+    @conversion_database = conversion_database
+  end
 
-    def convert
-        Quantity.new(
-            @initial_quantity.amount * conversion_factor(from: @initial_quantity.unit, to: @target_unit), @target_unit
-        )
-    end
+  def convert
+    Quantity.new(
+      amount: initial_quantity.amount * conversion_ratio,
+      unit: target_unit
+    )
+  end
 
-    private
+  private
 
-    attr_reader :conversion_database, :initial_quantity, :target_unit
+  attr_reader :conversion_database, :initial_quantity, :target_unit
 
-    def conversion_factor
-       conversion_database.conversion_ratio(
-           from: initial_quantity.unit,
-           to: target_unit
-       )
-    end
+  def conversion_ratio
+    conversion_database.conversion_ratio(
+      from: initial_quantity.unit,
+      to: target_unit
+    )
+  end
 end
